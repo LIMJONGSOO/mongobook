3e774fb24c56ec14e133a2ef1fedbc8cc620a96e
LIMJONGSOO <limjs4535@gmail.com>
2020-04-29 23:24:24 +0900

    구조 구성 중

---
+++ src/components/main/main.tsx
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import logo from 'assets/images/logo.png';
+import './App.scss';
+
+class Main extends Component {
+  state = {users: []}
+
+  conponentDidMount() {
+    fetch('/users')
+      .then(res => res.json())
+      .then(users => this.setState({users}));
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <h1>Users</h1>
+        {this.state.users.map(user => 
+          <div key={user._id}>{user.name} {user.age}</div>
+        )}
+      </div>
+    )
+  };
+}
+
+export default Main;
